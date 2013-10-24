4bb2690f08877ed7a7315e27bad1c61f2afea4b6
guidocelada <celadaguido@gmail.com>
2013-10-24 20:53:08 -0300

    1st

---
+++ src/remote/IfaceRemoteServer.java
@@ -0,0 +1,15 @@
+package remote;
+
+import java.rmi.Remote;
+
+/**
+ * Implementors of this interface can be 
+ * referenced and invoked by a remote machine.
+ * 
+ * @author Celada, Soria
+ *
+ */
+public interface IfaceRemoteServer extends Remote {
+    
+    public byte[] 
+}
