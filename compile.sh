55b8d027e507121a9c06a99c5cb4300dcc3ba651
Mauro Soria <maurosoria@gmail.com>
2013-10-25 06:55:41 -0300

    aa

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Compile Server
-rmic remote.RemoteServer
-javac remote/*.java
-jar cfe server.jar remote.StartRemoteObject remote/*.class
-
-#Compile client
-javac local/*.java
-jar cfe client.jar local.AskRemote local/*.class remote/IfaceRemoteServer.class
