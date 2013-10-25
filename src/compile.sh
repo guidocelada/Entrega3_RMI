902441fa1abb5f441d60fe42b6c09f471b2a6150
Mauro Soria <maurosoria@gmail.com>
2013-10-25 08:56:38 -0300

    agregando threads

--- src/compile.sh
+++ src/compile.sh
@@ -7,4 +7,8 @@ jar cfe server.jar remote.StartRemoteObject remote/*.class
 
 #Compile client
 javac local/*.java
-jar cfe client.jar local.AskRemote local/*.class remote/IfaceRemoteServer.class
+jar cfe client.jar local.AskRemote local/*.class remote/IfaceRemoteServer.class remote/RemoteServer_Stub.class
+
+#Compile MultiThreadTest
+javac thread_test/MultiThreadClientTest.java
+jar cfe thread_test.jar thread_test.MultiThreadClientTest thread_test/*.class remote/IfaceRemoteServer.class remote/RemoteServer_Stub.class
